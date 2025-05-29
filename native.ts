2759def568acdc1c16ac92b4b204e575b74b2cc3
Joona <69722179+Masterjoona@users.noreply.github.com>
2025-05-29 14:38:51 +0300

    prepare for vencord csp change

---
+++ native.ts
@@ -0,0 +1,20 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { RendererSettings } from "@main/settings";
+import { IpcMainInvokeEvent } from "electron";
+
+// @ts-ignore
+import("@main/csp").then(({ CspPolicies, ConnectSrc }) => {
+    const settings = RendererSettings.store.plugins?.SpotifyLyrics;
+    if (settings?.enabled) {
+        CspPolicies["lrclib.net"] = ConnectSrc;
+        CspPolicies["spotify-lyrics-api-pi.vercel.app"] = ConnectSrc;
+    }
+}).catch(() => { });
+
+
+export function dummy(e: IpcMainInvokeEvent) { }
